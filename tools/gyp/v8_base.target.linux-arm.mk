5e8d30763e36bc3a3e18f69ce1c9d014bbe4afd4
Android Chromium Automerger <chromium-automerger@android>
2013-02-22 05:17:35 +0000

    Update makefiles after merge of Chromium at r184032
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: I696eb21bc7527daf0cf9c720e070e5170902c07b

--- tools/gyp/v8_base.target.linux-arm.mk
+++ tools/gyp/v8_base.target.linux-arm.mk
@@ -71,6 +71,7 @@ LOCAL_SRC_FILES := \
 	v8/src/global-handles.cc \
 	v8/src/handles.cc \
 	v8/src/heap-profiler.cc \
+	v8/src/heap-snapshot-generator.cc \
 	v8/src/heap.cc \
 	v8/src/hydrogen-instructions.cc \
 	v8/src/hydrogen.cc \
