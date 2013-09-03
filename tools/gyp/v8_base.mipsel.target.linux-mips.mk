5a97231e3b44d422b0df6c2c7aa367147974255a
Android Chromium Automerger <chromium-automerger@android>
2013-09-03 18:57:44 +0000

    Update makefiles after merge of Chromium at r220988
    This commit was generated by merge_from_chromium.py.
    
    Change-Id: If52d693f492fa70691744b8df81cb67f1cb03b23

--- tools/gyp/v8_base.mipsel.target.linux-mips.mk
+++ tools/gyp/v8_base.mipsel.target.linux-mips.mk
@@ -122,7 +122,9 @@ LOCAL_SRC_FILES := \
 	v8/src/optimizing-compiler-thread.cc \
 	v8/src/parser.cc \
 	v8/src/platform/time.cc \
+	v8/src/platform/condition-variable.cc \
 	v8/src/platform/mutex.cc \
+	v8/src/platform/semaphore.cc \
 	v8/src/preparse-data.cc \
 	v8/src/preparser.cc \
 	v8/src/prettyprinter.cc \
